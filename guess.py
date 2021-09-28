30d77e967c6a4da413c6604df15cd67ec1ea1b82
Nicolas Nunes <n.nunes@testamento.fr>
2021-09-28 22:34:09 +0200

    test commit

--- guess.py
+++ guess.py
@@ -3,7 +3,7 @@ import random
 def guess(x):
     user_name = str(input("User name: "))
     random_number = random.randint(1, x)
-    guess = 0
+    guess = 0 
 
     while guess != random_number:
         guess = int(input(f"Guess a number between 1 and {x}: "))
