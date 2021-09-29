3560b6c229755dc216ac35dad0109fccfeb625a3
nnsncl <nuniroland@gmail.com>
2021-09-29 21:01:22 +0200

    List Methods (2)

--- methods.py
+++ methods.py
@@ -18,3 +18,15 @@ print(areas.index(20.0))
 
 # Print out how often 9.50 appears in areas
 print(areas.count(9.50))
+
+# Use append twice to add poolhouse and garage size
+areas.append(24.5); areas.append(15.45)
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
