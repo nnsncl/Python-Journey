20994aa96b7dee16c5413f54a0b586c0499601e2
nnsncl <nuniroland@gmail.com>
2021-09-29 20:55:44 +0200

    List Methods

--- methods.py
+++ methods.py
@@ -9,3 +9,12 @@ print(place); print(place_up)
 
 # Print out the number of o's in place
 print(place.count('o'))
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
