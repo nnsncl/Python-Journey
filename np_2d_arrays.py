2e91abf154a07d0ae43726daed68b333482a92e4
nnsncl <nuniroland@gmail.com>
2021-10-06 22:56:18 +0200

    ex.2 2d arrays

---
+++ np_2d_arrays.py
@@ -0,0 +1,40 @@
+
+# Import numpy
+import numpy as np
+
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Import numpy
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+# Create np_baseball (2 cols)
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49,:])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:, 1]
+
+# Print out height of 124th player
+print(np_baseball[123, 0])
