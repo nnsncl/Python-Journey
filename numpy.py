cd0e2061f131b0578516b0152f94fdbb7213cff0
nnsncl <nuniroland@gmail.com>
2021-10-06 22:30:48 +0200

    ex.4 numpy

--- numpy.py
+++ numpy.py
@@ -15,10 +15,6 @@ print(type(np_baseball))
 
 #height_in is already available and the numpy package is loaded, so you can start straight away (Source: stat.ucla.edu).
 # height is available as a regular list
-
-# Import numpy
-import numpy as np
-
 # Create a numpy array from height_in: np_height_in
 np_height_in = np.array(height_in)
 
@@ -38,8 +34,6 @@ print(np_height_m)
 
 # height and weight are available as regular lists
 
-# Import numpy
-import numpy as np
 
 # Create array from height_in with metric units: np_height_m
 np_height_m = np.array(height_in) * 0.0254
@@ -54,10 +48,6 @@ bmi = np_weight_kg / (np_height_m ** 2)
 print(bmi)
 
 # height and weight are available as a regular lists
-
-# Import numpy
-import numpy as np
-
 # Calculate the BMI: bmi
 np_height_m = np.array(height_in) * 0.0254
 np_weight_kg = np.array(weight_lb) * 0.453592
@@ -71,3 +61,15 @@ print(light)
 
 # Print out BMIs of all baseball players whose BMI is below 21
 print(bmi[light])
+
+# height and weight are available as a regular lists
+
+# Store weight and height lists as numpy arrays
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
+# Print out the weight at index 50
+print(np_weight_lb[50])
+
+# Print out sub-array of np_height_in: index 100 up to and including index 110
+print(np_height_in[100:111])
