d3ba06e499c582e15e0cfa239e3a32fd2cd739f4
nnsncl <nuniroland@gmail.com>
2021-10-10 16:00:00 +0200

    loc, iloc practice

--- intermediate/pandas.py
+++ intermediate/pandas.py
@@ -46,3 +46,24 @@ print(cars[:3])
 
 # Print out fourth, fifth and sixth observation
 print(cars[3:6])
+
+# Print out observation for Japan
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
+
+# Print out drives_right value of Morocco
+print(cars.loc['MOR', 'drives_right'])
+
+# Print sub-DataFrame
+print(cars.loc[['RU', 'MOR'], ['country', 'drives_right']])
+
+# Print out drives_right column as Series
+print(cars.iloc[:, 2])
+
+# Print out drives_right column as DataFrame
+print(cars.iloc[:, [2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:, ['cars_per_cap', 'drives_right']])
