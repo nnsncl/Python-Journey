a5d4295a3b300e7c9c9a91610886c73fbc9b9d47
nnsncl <nuniroland@gmail.com>
2021-10-10 13:49:48 +0200

    matplot intro

--- intermediate/matplot.py
+++ intermediate/matplot.py
@@ -9,3 +9,97 @@ plt.show()
 plt.scatter(gdp_cap, life_exp)
 plt.xscale('log') # Put the x-axis on a logarithmic scale
 plt.show()
+
+
+# Create histogram of life_exp data
+plt.hist(life_exp) # 10 by default
+
+# Display histogram
+plt.show()
+
+# Build histogram with 5 bins
+plt.hist(life_exp, 5)
+
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp, 20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+# Histogram of life_exp, 15 bins
+plt.hist(life_exp, 15)
+
+# Show and clear plot
+plt.show()
+plt.clf()
+
+# Histogram of life_exp1950, 15 bins
+plt.hist(life_exp1950, 15)
+
+# Show and clear plot again
+plt.show()
+plt.clf()
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
+
+
+# Scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+
+# Definition of tick_val and tick_lab
+tick_val = [1000, 10000, 100000]
+tick_lab = ['1k', '10k', '100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks(tick_val, tick_lab)
+
+# After customizing, display the plot
+plt.show()
+
+# Import numpy as np
+import numpy as np
+
+# Specify c and alpha inside plt.scatter()
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col, alpha = 0.8)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+plt.text(1550, 71, 'India')
+plt.text(5700, 80, 'China')
+plt.grid(True)
+
+# Show the plot
+plt.show()
