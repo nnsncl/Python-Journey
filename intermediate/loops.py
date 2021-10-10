b3234926e54f33a8d6358172302fcb26eafce6a4
nnsncl <nuniroland@gmail.com>
2021-10-10 22:19:20 +0200

    loop over arrays and dict

--- intermediate/loops.py
+++ intermediate/loops.py
@@ -18,3 +18,35 @@ while offset != 0 :
     else : 
       offset = offset - 1  
     print(offset)
+
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for area in areas :
+    print(area)
+
+
+# Change for loop to use enumerate() and update print()
+for index, a in enumerate(areas) :
+    print("room " + str(index + 1) + ": " + str(area))
+
+    # house list of lists
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
+         
+# Build a for loop from scratch
+for room, area in house :
+    print("the " + str(room) + " is " + str(area) + " sqm")
+
+    # Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
+          
+# Iterate over europe
+for key, value in europe.items() :
+    print("the capital of " + key + " is " + value)
