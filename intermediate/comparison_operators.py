2ef988222bc5420189b332d8b3debc29a2aa5b6c
nnsncl <nuniroland@gmail.com>
2021-10-10 21:28:25 +0200

    comparison operators on DataFrames

--- intermediate/comparison_operators.py
+++ intermediate/comparison_operators.py
@@ -57,3 +57,39 @@ print(np.logical_or(my_house > 18.5, my_house < 10))
 
 # Both my_house and your_house smaller than 11
 print(np.logical_and(my_house < 11, your_house < 11))
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Extract drives_right column as Series: dr
+# dr = cars['drives_right'] == True
+
+# Use dr to subset cars: sel
+# sel = cars[dr]
+
+# Convert code to a one-liner
+sel = cars[cars['drives_right']]
+
+# Print sel
+print(sel)
+
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc = cars['cars_per_cap']
+many_cars = cpc > 500
+car_maniac = cars[many_cars]
+
+# Print car_maniac
+print(car_maniac)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc > 100, cpc < 500)
+medium = cars[between]
+
+# Print medium
+print(medium)
