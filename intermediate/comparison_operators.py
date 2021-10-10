c223f4b5f4de3c5fecc254b83bf1034789e01958
nnsncl <nuniroland@gmail.com>
2021-10-10 20:21:18 +0200

    Compare types

---
+++ intermediate/comparison_operators.py
@@ -0,0 +1,24 @@
+# Comparison of booleans
+True == False
+
+# Comparison of integers
+-5 * 15 != 75
+
+# Comparison of strings
+"pyscript" == "PyScript"
+
+# Compare a boolean with an integer
+True == 1
+
+# Comparison of integers
+x = -3 * 6
+
+
+# Comparison of strings
+y = "test"
+
+
+# Comparison of booleans
+print(x >= -10)
+print("test" <= y)
+print(True > False)
