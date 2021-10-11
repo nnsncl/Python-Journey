34a774fbcb305fff42a9aef9c4391135f0353123
nnsncl <nuniroland@gmail.com>
2021-10-11 15:36:11 +0200

    Manipulate dict with csv

--- data-types/dictionaries.py
+++ data-types/dictionaries.py
@@ -84,3 +84,36 @@ else:
 if 5 in baby_names[2013]:
    # Print 'Found Rank 5'
    print('Found Rank 5')
+
+
+# Import the python CSV module
+import csv
+
+# Create a python file object in read mode for the baby_names.csv file: csvfile
+csvfile = open('baby_names.csv', 'r')
+
+# Loop over a csv reader on the file object
+for row in csv.reader(csvfile):
+    # Print each row 
+    print(row)
+    # Add the rank and name to the dictionary
+    baby_names[row[5]] = row[3]
+
+# Print the dictionary keys
+print(baby_names.keys())
+
+# Import the python CSV module
+import csv
+
+# Create a python file object in read mode for the `baby_names.csv` file: csvfile
+csvfile = open('baby_names.csv', 'r')
+
+# Loop over a DictReader on the file
+for row in csv.DictReader(csvfile):
+    # Print each row 
+    print(row)
+    # Add the rank and name to the dictionary: baby_names
+    baby_names[row['RANK']] = row['NAME']
+
+# Print the dictionary keys
+print(baby_names.keys())
