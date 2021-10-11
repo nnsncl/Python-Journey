4986a33f6db313fe41de2b1aead321ebe09afac3
nnsncl <nuniroland@gmail.com>
2021-10-11 14:58:38 +0200

    alter dict w/get, update, pop methods

--- data-types/dictionaries.py
+++ data-types/dictionaries.py
@@ -30,3 +30,29 @@ print(boy_names[2013].keys())
 for year in boy_names:
     # Safely print the year and the third ranked name or 'Unknown'
     print(year, boy_names[year].get(3, 'Unknown'))
+
+# Assign the names_2011 dictionary as the value to the 2011 key of boy_names
+boy_names[2011] = names_2011
+
+# Update the 2012 key in the boy_names dictionary
+boy_names[2012].update([(1, 'Casey'), (2, 'Aiden')])
+
+# Loop over the years in the boy_names dictionary 
+for year in boy_names:
+    # Sort the data for each year by descending rank and get the lowest one
+    lowest_ranked = sorted(boy_names[year], reverse=True)[0]
+    # Safely print the year and the least popular name or 'Not Available'
+    print(year, boy_names[year].get(lowest_ranked, 'Not Available'))
+
+
+# Remove 2011 from female_names and store it: female_names_2011
+female_names_2011 = female_names.pop(2011)
+
+# Safely remove 2015 from female_names with an empty dictionary as the default: female_names_2015
+female_names_2015 = female_names.pop(2015, {})
+
+# Delete 2012 from female_names
+del female_names[2012]
+
+# Print female_names
+print(female_names)
