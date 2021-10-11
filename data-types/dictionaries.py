fa66f9f2edf7a5f466098b099bf47eaf84df1fbd
nnsncl <nuniroland@gmail.com>
2021-10-11 14:29:55 +0200

    SAfely access dct values

--- data-types/dictionaries.py
+++ data-types/dictionaries.py
@@ -10,3 +10,23 @@ for rank, name in female_baby_names_2012.items():
 for rank in sorted(names_by_rank, reverse=True)[:10]:
     # Print each item
     print(names_by_rank[rank])
+
+# Safely print rank 7 from the names dictionary
+print(names.get(7))
+
+# Safely print the type of rank 100 from the names dictionary
+print(type(names.get(100)))
+
+# Safely print rank 105 from the names dictionary or 'Not Found'
+print(names.get(105, 'Not Found'))
+
+# Print a list of keys from the boy_names dictionary
+print(boy_names.keys())
+
+# Print a list of keys from the boy_names dictionary for the year 2013
+print(boy_names[2013].keys())
+
+# Loop over the dictionary
+for year in boy_names:
+    # Safely print the year and the third ranked name or 'Unknown'
+    print(year, boy_names[year].get(3, 'Unknown'))
