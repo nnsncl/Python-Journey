a6259ffef8612e5abc5399a80a1a97820ca6f044
nnsncl <nuniroland@gmail.com>
2021-10-11 13:53:30 +0200

    separate files

--- data-types/lists.py
+++ data-types/lists.py
@@ -38,21 +38,3 @@ for name in sorted(baby_names):
 # Pair up the girl and boy names: pairs
 pairs = zip(girl_names, boy_names)
 
-# Iterate over pairs
-for idx, pair in enumerate(pairs):
-    # Unpack pair: girl_name, boy_name
-    girl_name, boy_name = pair
-    # Print the rank and names associated with each rank
-    print('Rank {}: {} and {}'.format(idx, girl_name, boy_name))
-
-
-# Create the normal variable: normal
-normal = 'simple'
-
-# Create the mistaken variable: error
-error = 'trailing comma',
-
-# Print the types of the variables
-print(type(normal))
-print(type(error))
-
---
+++ data-types/tuples.py
@@ -0,0 +1,18 @@
+
+# Iterate over pairs
+for idx, pair in enumerate(pairs):
+    # Unpack pair: girl_name, boy_name
+    girl_name, boy_name = pair
+    # Print the rank and names associated with each rank
+    print('Rank {}: {} and {}'.format(idx, girl_name, boy_name))
+
+
+# Create the normal variable: normal
+normal = 'simple'
+
+# Create the mistaken variable: error
+error = 'trailing comma',
+
+# Print the types of the variables
+print(type(normal))
+print(type(error))
