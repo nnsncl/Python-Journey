3e2658e9618d92123f3417030e3d58f33280c96e
nnsncl <nuniroland@gmail.com>
2021-10-11 22:21:40 +0200

    namedtuples pratice

--- collections-module/collections.py
+++ collections-module/collections.py
@@ -71,3 +71,32 @@ print(list(ridership_date.keys())[-1])
 
 # Pop the last item from ridership_date and print it
 print(ridership_date.popitem())
+
+# Import namedtuple from collections
+from collections import namedtuple
+
+# Create the namedtuple: DateDetails
+DateDetails = namedtuple('DateDetails', ['date', 'stop', 'riders'])
+
+# Create the empty list: labeled_entries
+labeled_entries = []
+
+# Iterate over the entries list
+for date, stop, riders in entries:
+    # Append a new DateDetails namedtuple instance for each entry to labeled_entries
+    labeled_entries.append(DateDetails(date, stop, riders))
+    
+# Print the first 5 items in labeled_entries
+print(labeled_entries[:5])
+
+
+# Iterate over the first twenty items in labeled_entries
+for item in labeled_entries[:20]:
+    # Print each item's stop
+    print(item.stop)
+
+    # Print each item's date
+    print(item.date)
+
+    # Print each item's riders
+    print(item.riders)
