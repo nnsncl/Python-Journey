ead58b1df65df9f7c475719614659e3ee2f04db7
nnsncl <nuniroland@gmail.com>
2021-10-11 13:48:15 +0200

    tuples practice

--- data-types/lists.py
+++ data-types/lists.py
@@ -25,12 +25,34 @@ print(baby_names)
 # Create the empty list: baby_names
 baby_names = []
 
-# Loop over records 
+# Loop over records
 for row in records:
     # Add the name to the list
     baby_names.append(row[3])
-    
+
 # Sort the names in alphabetical order
 for name in sorted(baby_names):
     # Print each name
     print(name)
+
+# Pair up the girl and boy names: pairs
+pairs = zip(girl_names, boy_names)
+
+# Iterate over pairs
+for idx, pair in enumerate(pairs):
+    # Unpack pair: girl_name, boy_name
+    girl_name, boy_name = pair
+    # Print the rank and names associated with each rank
+    print('Rank {}: {} and {}'.format(idx, girl_name, boy_name))
+
+
+# Create the normal variable: normal
+normal = 'simple'
+
+# Create the mistaken variable: error
+error = 'trailing comma',
+
+# Print the types of the variables
+print(type(normal))
+print(type(error))
+
