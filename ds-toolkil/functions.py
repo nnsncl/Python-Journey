eafbdef0ed7c00d428c79f42824f24e2eedbfa64
nnsncl <nuniroland@gmail.com>
2021-10-12 16:24:11 +0200

    var-leng args on funcs

--- ds-toolkil/functions.py
+++ ds-toolkil/functions.py
@@ -265,3 +265,28 @@ big_no_echo = shout_echo("Hey", intense=True)
 # Print values
 print(with_big_echo)
 print(big_no_echo)
+
+
+# Define gibberish
+def gibberish(*args):
+    """Concatenate strings in *args together."""
+
+    # Initialize an empty string: hodgepodge
+    hodgepodge= str()
+
+    # Concatenate the strings in args
+    for word in args:
+        hodgepodge += word
+
+    # Return hodgepodge
+    return hodgepodge
+
+# Call gibberish() with one string: one_word
+one_word = gibberish('luke')
+
+# Call gibberish() with five strings: many_words
+many_words = gibberish("luke", "leia", "han", "obi", "darth")
+
+# Print one_word and many_words
+print(one_word)
+print(many_words)
