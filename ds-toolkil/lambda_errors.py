413d975604545da8a4d4ae0230603557dbbe72b8
nnsncl <nuniroland@gmail.com>
2021-10-12 21:29:33 +0200

    lambda basics with map, filter and reduce methods

--- ds-toolkil/lambda_errors.py
+++ ds-toolkil/lambda_errors.py
@@ -38,3 +38,27 @@ shout_spells_list = list(shout_spells)
 
 # Print the result
 print(shout_spells_list)
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'pippin', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf']
+
+# Use filter() to apply a lambda function over fellowship: result
+result = filter(lambda member: len(member) > 6, fellowship)
+
+# Convert result to a list: result_list
+result_list = list(result)
+
+# Print result_list
+print(result_list)
+
+# Import reduce from functools
+from functools import reduce
+
+# Create a list of strings: stark
+stark = ['robb', 'sansa', 'arya', 'brandon', 'rickon']
+
+# Use reduce() to apply a lambda function over stark: result
+result = reduce(lambda item1, item2: item1 + item2, stark)
+
+# Print the result
+print(result)
