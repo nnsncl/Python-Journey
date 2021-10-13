6604d55c511df4ee2dacd08b7deea98a0d87a1e2
nnsncl <nuniroland@gmail.com>
2021-10-13 13:19:03 +0200

    conditional in comprehensions

--- ds-toolkil/list_comprehensions.py
+++ ds-toolkil/list_comprehensions.py
@@ -7,3 +7,13 @@ matrix = [[col for col in range(0, 5)] for row in range(0, 5)]
 # Print the matrix
 for row in matrix:
     print(row)
+
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member for member in fellowship if len(member) >= 7]
+
+# Print the new list
+print(new_fellowship)
