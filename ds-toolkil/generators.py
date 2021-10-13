9e3797b6b4dcbf928ebce0196ebbc430ae173957
nnsncl <nuniroland@gmail.com>
2021-10-13 15:45:49 +0200

    changing the output in generator expressions

--- ds-toolkil/generators.py
+++ ds-toolkil/generators.py
@@ -11,3 +11,14 @@ print(next(result))
 # Print the rest of the values
 for value in result:
     print(value)
+
+
+# Create a list of strings: lannister
+lannister = ['cersei', 'jaime', 'tywin', 'tyrion', 'joffrey']
+
+# Create a generator object: lengths
+lengths = (len(person) for person in lannister)
+
+# Iterate over and print the values in lengths
+for value in lengths:
+    print(value)
