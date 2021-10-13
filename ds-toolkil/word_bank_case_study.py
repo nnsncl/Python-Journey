471ed4ad04a1e119f7ac8d078af7744d34689e9d
nnsncl <nuniroland@gmail.com>
2021-10-13 17:02:59 +0200

    load data in chink with generator

--- ds-toolkil/word_bank_case_study.py
+++ ds-toolkil/word_bank_case_study.py
@@ -1,4 +1,7 @@
-@# Zip lists: zipped_lists
+import pandas as pd
+
+
+@  # Zip lists: zipped_lists
 zipped_lists = zip(feature_names, row_vals)
 
 # Create a dictionary: rs_dict
@@ -22,6 +25,7 @@ def lists2dict(list1, list2):
     # Return the dictionary
     return rs_dict
 
+
 # Call lists2dict: rs_fxn
 rs_fxn = lists2dict(feature_names, row_vals)
 
@@ -42,7 +46,6 @@ print(list_of_dicts[0])
 print(list_of_dicts[1])
 
 # Import the pandas package
-import pandas as pd
 
 # Turn list of lists into list of dicts: list_of_dicts
 list_of_dicts = [lists2dict(feature_names, sublist) for sublist in row_lists]
@@ -52,3 +55,84 @@ df = pd.DataFrame(list_of_dicts)
 
 # Print the head of the DataFrame
 print(df.head())
+
+
+# Open a connection to the file
+with open('world_dev_ind.csv') as file:
+
+    # Skip the column names
+    file.readline()
+
+    # Initialize an empty dictionary: counts_dict
+    counts_dict = {}
+
+    # Process only the first 1000 rows
+    for j in range(1000):
+
+        # Split the current line into a list: line
+        line = file.readline().split(',')
+
+        # Get the value for the first column: first_col
+        first_col = line[0]
+
+        # If the column value is in the dict, increment its value
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+
+        # Else, add to the dict and set value to 1
+        else:
+            counts_dict[first_col] = 1
+
+# Print the resulting dictionary
+print(counts_dict)
+
+
+# Define read_large_file()
+def read_large_file(file_object):
+    """A generator function to read a large file lazily."""
+
+    # Loop indefinitely until the end of the file
+    while True:
+
+        # Read a line from the file: data
+        data = file_object.readline()
+
+        # Break if this is the end of the file
+        if not data:
+            break
+
+        # Yield the line of data
+        yield data
+
+
+# Open a connection to the file
+with open('world_dev_ind.csv') as file:
+
+    # Create a generator object for the file: gen_file
+    gen_file = read_large_file(file)
+
+    # Print the first three lines of the file
+    print(next(gen_file))
+    print(next(gen_file))
+    print(next(gen_file))
+
+
+# Initialize an empty dictionary: counts_dict
+counts_dict = {}
+
+# Open a connection to the file
+with open('world_dev_ind.csv') as file:
+
+    # Iterate over the generator from read_large_file()
+    for line in read_large_file(file):
+
+        row = line.split(',')
+        first_col = row[0]
+
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+        else:
+            counts_dict[first_col] = 1
+
+# Print
+print(counts_dict)
