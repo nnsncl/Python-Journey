bc028355cc6c00a7e467aa3f29483e4c4e89f062
nnsncl <nuniroland@gmail.com>
2021-10-13 13:15:30 +0200

    nested comprehension lists with range

--- ds-toolkil/list_comprehensions.py
+++ ds-toolkil/list_comprehensions.py
@@ -1,2 +1,9 @@
 # Create list comprehension: squares
 squares = [i ** 2 for i in range(0, 10)]
+
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(0, 5)] for row in range(0, 5)]
+
+# Print the matrix
+for row in matrix:
+    print(row)
