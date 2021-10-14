26ac2f7ed30ccee6d5cb3caf90d53cede9ecf840
nnsncl <nuniroland@gmail.com>
2021-10-14 22:35:34 +0200

    Map builtin practice

--- efficient_python/foundations.py
+++ efficient_python/foundations.py
@@ -51,3 +51,15 @@ print(indexed_names_comp)
 # Unpack an enumerate object with a starting index of one
 indexed_names_unpack = [*enumerate(names, 1)]
 print(indexed_names_unpack)
+
+# Use map to apply str.upper to each element in names
+names_map = map(str.upper, names)
+
+# Print the type of the names_map
+print(type(names_map))
+
+# Unpack names_map into a list
+names_uppercase = [*names_map]
+
+# Print the list created above
+print(names_uppercase)
