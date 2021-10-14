d24537a8add40ffb34f740ef1075ecfc3d63bf6b
nnsncl <nuniroland@gmail.com>
2021-10-14 22:24:53 +0200

    Built-in practice: enumerate()

--- efficient_python/foundations.py
+++ efficient_python/foundations.py
@@ -27,5 +27,27 @@ nums_list = list(nums)
 print(nums_list)
 
 # Create a new list of odd numbers from 1 to 11 by unpacking a range object
-nums_list2 = [*range(1, 12, + 2)]
+nums_list2 = [*range(1, 12, 2)]
 print(nums_list2)
+
+
+
+
+# Create another list (indexed_names_unpack) by using the star character (*)\
+# to unpack the enumerate object created from using enumerate() on names\
+# This time, start the index for enumerate() at one instead of zero.
+
+# Rewrite the for loop to use enumerate
+indexed_names = []
+for i, name in enumerate(names):
+    index_name = (i,name)
+    indexed_names.append(index_name) 
+print(indexed_names)
+
+# Rewrite the above for loop using list comprehension
+indexed_names_comp = [(i, name) for i,name in enumerate(names)]
+print(indexed_names_comp)
+
+# Unpack an enumerate object with a starting index of one
+indexed_names_unpack = [*enumerate(names, 1)]
+print(indexed_names_unpack)
