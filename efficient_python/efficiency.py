b31cdb1a79585b9194b2969399094aa9cdcd0d00
nnsncl <nuniroland@gmail.com>
2021-10-15 16:10:30 +0200

    set theory practice

--- efficient_python/efficiency.py
+++ efficient_python/efficiency.py
@@ -66,3 +66,43 @@ print(ash_only)
 # Find the Pokémon that are in only one set (not both)
 unique_to_set = ash_set.symmetric_difference(misty_set)
 print(unique_to_set)
+
+# Convert Brock's Pokédex to a set
+brock_pokedex_set = set(brock_pokedex)
+print(brock_pokedex_set)
+
+# Check if Psyduck is in Ash's list and Brock's set
+print('Psyduck' in ash_pokedex)
+print('Psyduck' in brock_pokedex_set)
+
+# Check if Machop is in Ash's list and Brock's set
+print('Machop' in ash_pokedex)
+print('Machop' in brock_pokedex_set)
+
+#Using set is faster than an list
+
+# NOT RECOMANDED APPROACH WITH A FOR LOOP
+def find_unique_items(data):
+    uniques = []
+
+    for item in data:
+        if item not in uniques:
+            uniques.append(item)
+
+    return uniques
+
+# Use find_unique_items() to collect unique Pokémon names
+uniq_names_func = find_unique_items(names)
+print(len(uniq_names_func))
+
+# Convert the names list to a set to collect unique Pokémon names
+uniq_names_set = set(names)
+print(len(uniq_names_set))
+
+# Check that both unique collections are equivalent
+print(sorted(uniq_names_func) == sorted(uniq_names_set))
+
+# Use the best approach to collect unique primary types and generations
+uniq_types = set(primary_types) 
+uniq_gens = set(generations)
+print(uniq_types, uniq_gens, sep='\n') 
