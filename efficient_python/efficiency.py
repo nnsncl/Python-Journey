7e9b115ce5624893375056c06e373d9658136c23
nnsncl <nuniroland@gmail.com>
2021-10-15 15:58:52 +0200

    comparing methods

--- efficient_python/efficiency.py
+++ efficient_python/efficiency.py
@@ -50,3 +50,19 @@ print(combos_2, '\n')
 # Collect all possible combinations of 4 Pokémon directly into a list
 combos_4 = [*combinations(pokemon, 4)]
 print(combos_4)
+
+# Convert both lists to sets
+ash_set = set(ash_pokedex)
+misty_set = set(misty_pokedex)
+
+# Find the Pokémon that exist in both sets
+both = ash_set.intersection(misty_set)
+print(both)
+
+# Find the Pokémon that Ash has and Misty does not have
+ash_only = ash_set.difference(misty_set)
+print(ash_only)
+
+# Find the Pokémon that are in only one set (not both)
+unique_to_set = ash_set.symmetric_difference(misty_set)
+print(unique_to_set)
