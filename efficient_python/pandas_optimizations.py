e0b9f94ad830651a4f012fbf9948d2ead955dc0f
nnsncl <nuniroland@gmail.com>
2021-10-15 17:42:56 +0200

    itertuples practice

--- efficient_python/pandas_optimizations.py
+++ efficient_python/pandas_optimizations.py
@@ -42,3 +42,20 @@ for row in rangers_df.itertuples():
   # Check if rangers made Playoffs (1 means yes; 0 means no)
   if row.Playoffs == 1:
     print(i, year, wins)
+
+
+run_diffs = []
+
+# Loop over the DataFrame and calculate each row's run differential
+for row in yankees_df.itertuples():
+    
+    runs_scored = row.RS
+    runs_allowed = row.RA
+
+    run_diff = calc_run_diff(runs_scored, runs_allowed)
+    
+    run_diffs.append(run_diff)
+
+# Append new column
+yankees_df['RD'] = run_diffs
+print(yankees_df)
