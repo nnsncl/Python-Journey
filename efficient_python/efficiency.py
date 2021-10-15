6947bf1223ec2978f17a9cfd52f01324f6c7e014
nnsncl <nuniroland@gmail.com>
2021-10-15 15:40:47 +0200

    zip, count and combination

---
+++ efficient_python/efficiency.py
@@ -0,0 +1,52 @@
+# Three lists have been loaded into your session from a dataset that contains 720 Pokémon:
+
+# The names list contains the names of each Pokémon.
+# The primary_types list contains the corresponding primary type of each Pokémon.
+# The secondary_types list contains the corresponding secondary type of each Pokémon (nan if the Pokémon has only one type).
+# We want to combine each Pokémon's name and types together so that you easily see a description of each Pokémon. Practice using zip() to accomplish this task.
+
+
+# Combine names and primary_types with zip()
+names_type1 = [*zip(names, primary_types)]
+
+print(*names_type1[:5], sep='\n')
+
+# Combine all three lists together
+names_types = [*zip(names, primary_types, secondary_types)]
+
+print(*names_types[:5], sep='\n')
+
+# Combine five items from names and three items from primary_types
+differing_lengths = [*zip(names[0: 5], primary_types[0: 3])]
+
+print(*differing_lengths, sep='\n')
+
+# Collect the count of primary types
+type_count = Counter(primary_types)
+print(type_count, '\n')
+
+# Collect the count of generations
+gen_count = Counter(generations)
+print(gen_count, '\n')
+
+# Use list comprehension to get each Pokémon's starting letter
+starting_letters = [name[0] for name in names]
+
+# Collect the count of Pokémon for each starting_letter
+starting_letters_count = Counter(starting_letters)
+print(starting_letters_count)
+
+# Import combinations from itertools
+from itertools import combinations
+
+# Create a combination object with pairs of Pokémon
+combos_obj = combinations(pokemon, 2)
+print(type(combos_obj), '\n')
+
+# Convert combos_obj to a list by unpacking
+combos_2 = [*combos_obj]
+print(combos_2, '\n')
+
+# Collect all possible combinations of 4 Pokémon directly into a list
+combos_4 = [*combinations(pokemon, 4)]
+print(combos_4)
