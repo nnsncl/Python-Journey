d3d96d207eaa28769fbbacb6b029ae163abda7ff
nnsncl <nuniroland@gmail.com>
2021-10-15 17:11:12 +0200

    writting efficient loops

--- efficient_python/efficiency.py
+++ efficient_python/efficiency.py
@@ -134,3 +134,51 @@ print(poke_list_np[:3])
 print(poke_list[:3], '\n')
 top_3 = sorted(poke_list_np, key=lambda x: x[1], reverse=True)[:3]
 print('3 strongest Pokémon:\n{}'.format(top_3))
+
+
+# Import Counter
+from collections import Counter
+
+# Collect the count of each generation
+gen_counts = Counter(generations)
+
+# Improve for loop by moving one calculation above the loop
+total_count = len(generations)
+
+for gen,count in gen_counts.items():
+    gen_percent = round(count / total_count * 100, 2)
+    print('generation {}: count = {:3} percentage = {}'
+          .format(gen, count, gen_percent))
+
+
+
+# Collect all possible pairs using combinations()
+possible_pairs = [*combinations(pokemon_types, 2)]
+
+# Create an empty list called enumerated_tuples
+enumerated_tuples = []
+
+# Append each enumerated_pair_tuple to the empty list above
+for i,pair in enumerate(possible_pairs, 1):
+    enumerated_pair_tuple = (i,) + pair
+    enumerated_tuples.append(enumerated_pair_tuple)
+
+# Convert all tuples in enumerated_tuples to a list
+enumerated_pairs = [*map(list, enumerated_tuples)]
+print(enumerated_pairs)
+
+
+# Calculate the total HP avg and total HP standard deviation
+hp_avg = hps.mean()
+hp_std = hps.std()
+
+# Use NumPy to eliminate the previous for loop
+z_scores = (hps - hp_avg)/hp_std
+
+# Combine names, hps, and z_scores
+poke_zscores2 = [*zip(names, hps, z_scores)]
+print(*poke_zscores2[:3], sep='\n')
+
+# Use list comprehension with the same logic as the highest_hp_pokemon code block
+highest_hp_pokemon2 = [(name, hp, zcore) for name, hp, zcore in poke_zscores2 if zcore > 2]
+print(*highest_hp_pokemon2, sep='\n')
