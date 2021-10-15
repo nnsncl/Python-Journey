f557401513e49df5114250b76507c95e99bb4cda
nnsncl <nuniroland@gmail.com>
2021-10-15 16:35:01 +0200

    gathering and storing with efficient loops

--- efficient_python/efficiency.py
+++ efficient_python/efficiency.py
@@ -106,3 +106,31 @@ print(sorted(uniq_names_func) == sorted(uniq_names_set))
 uniq_types = set(primary_types) 
 uniq_gens = set(generations)
 print(uniq_types, uniq_gens, sep='\n') 
+
+
+# Collect Pokémon that belong to generation 1 or generation 2
+gen1_gen2_pokemon = [name for name,gen in zip(poke_names, poke_gens) if gen < 3]
+
+# Create a map object that stores the name lengths
+name_lengths_map = map(len, poke_names)
+
+# Combine gen1_gen2_pokemon and name_lengths_map into a list
+gen1_gen2_name_lengths = [*zip(gen1_gen2_pokemon, name_lengths_map)]
+
+print(gen1_gen2_name_lengths_loop[:5])
+print(gen1_gen2_name_lengths[:5])
+
+# Create a total stats array
+total_stats_np = stats.sum(axis=1)
+
+# Create an average stats array
+avg_stats_np = stats.mean(axis=1)
+
+# Combine names, total_stats_np, and avg_stats_np into a list
+poke_list_np = [*zip(names, total_stats_np, avg_stats_np)]
+
+print(poke_list_np == poke_list, '\n')
+print(poke_list_np[:3])
+print(poke_list[:3], '\n')
+top_3 = sorted(poke_list_np, key=lambda x: x[1], reverse=True)[:3]
+print('3 strongest Pokémon:\n{}'.format(top_3))
