9dab67753d96fc3a5f92d0bb15798a28ee416fbb
nnsncl <nuniroland@gmail.com>
2021-10-15 17:37:38 +0200

    itertuples() practice

--- efficient_python/pandas_optimizations.py
+++ efficient_python/pandas_optimizations.py
@@ -31,3 +31,14 @@ for i,row in giants_df.iterrows():
 
 giants_df['RD'] = run_diffs
 print(giants_df)
+
+
+# Loop over the DataFrame and print each row's Index, Year and Wins (W)
+for row in rangers_df.itertuples():
+  i = row.Index
+  year = row.Year
+  wins = row.W
+  
+  # Check if rangers made Playoffs (1 means yes; 0 means no)
+  if row.Playoffs == 1:
+    print(i, year, wins)
