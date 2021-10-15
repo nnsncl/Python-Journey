5849f053cfc2c5ea117c5433d07b10d354a3ea80
nnsncl <nuniroland@gmail.com>
2021-10-15 18:00:15 +0200

    pandas alt to loops

--- efficient_python/pandas_optimizations.py
+++ efficient_python/pandas_optimizations.py
@@ -59,3 +59,42 @@ for row in yankees_df.itertuples():
 # Append new column
 yankees_df['RD'] = run_diffs
 print(yankees_df)
+
+
+
+
+
+# Analyzing baseball stats with .apply()
+
+# Gather sum of all columns
+stat_totals = rays_df.apply(sum, axis=0)
+print(stat_totals)
+
+# Gather total runs scored in all games per year
+total_runs_scored = rays_df[['RS', 'RA']].apply(sum, axis=1)
+print(total_runs_scored)
+
+# Convert numeric playoffs to text by applying text_playoffs()
+textual_playoffs = rays_df.apply(lambda row: text_playoffs(row['Playoffs']), axis=1)
+print(textual_playoffs)
+
+# Display the first five rows of the DataFrame
+print(dbacks_df.head())
+
+def calc_win_perc(wins, games_played):
+    win_perc = wins / games_played
+    return np.round(win_perc,2)
+
+# Display the first five rows of the DataFrame
+print(dbacks_df.head())
+
+# Create a win percentage Series 
+win_percs = dbacks_df.apply(lambda row: calc_win_perc(row['W'], row['G']), axis=1)
+print(win_percs, '\n')
+
+# Append a new column to dbacks_df
+dbacks_df['WP'] = win_percs
+print(dbacks_df, '\n')
+
+# Display dbacks_df where WP is greater than 0.50
+print(dbacks_df[dbacks_df['WP'] >= 0.50])
