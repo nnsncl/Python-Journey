71d9688e1ef3e19934f3ad4e0f5e2cc464165745
nnsncl <nuniroland@gmail.com>
2021-10-18 00:13:35 +0200

    replace() practice

--- efficient-core-pandas/replace-scalar-values.py
+++ efficient-core-pandas/replace-scalar-values.py
@@ -4,3 +4,11 @@ poker_hands['Class'].replace(1, -2, inplace=True)
 poker_hands['Class'].replace(2, -3, inplace=True)
 
 print(poker_hands[['Class', 'Explanation']])
+
+
+start_time = time.time()
+
+# Replace all the entries that has 'FEMALE' as a gender with 'GIRL'
+names['Gender'].replace('FEMALE', 'GIRL', inplace=True)
+
+print("Time using .replace(): {} sec".format(time.time() - start_time))
