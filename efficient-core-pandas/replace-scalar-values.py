b83a1e45a0c8e4e2309f755aab6e99cc9c0d1f47
nnsncl <nuniroland@gmail.com>
2021-10-18 00:26:33 +0200

    replace list  data w/ replace

--- efficient-core-pandas/replace-scalar-values.py
+++ efficient-core-pandas/replace-scalar-values.py
@@ -22,3 +22,11 @@ names['Ethnicity'].loc[(names['Ethnicity'] == 'BLACK NON HISP') |
                       (names['Ethnicity'] ==  'WHITE NON HISPANIC')] = 'NON HISPANIC'
 
 print("Time using .loc[]: sec".format(time.time() - start_time))
+
+
+start_time = time.time()
+
+# Replace all non-Hispanic ethnicities with 'NON HISPANIC'
+names['Ethnicity'].replace(['BLACK NON HISP', 'BLACK NON HISPANIC', 'WHITE NON HISP' , 'WHITE NON HISPANIC'], 'NON HISPANIC', inplace=True)
+
+print("Time using .replace(): {} sec".format(time.time() - start_time))
