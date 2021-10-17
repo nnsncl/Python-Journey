385638e25c8a74a8f8b2a68edd70bbdb641713a1
nnsncl <nuniroland@gmail.com>
2021-10-18 00:33:36 +0200

    replace single values on dict using replace()

--- efficient-core-pandas/replace-scalar-values.py
+++ efficient-core-pandas/replace-scalar-values.py
@@ -41,3 +41,7 @@ print("Time using .replace(): {} sec".format(time.time() - start_time))
 # Replace Royal flush or Straight flush to Flush
 poker_hands.replace({'Straight flush':'Flush', 'Royal flush':'Flush'}, inplace=True)
 print(poker_hands['Explanation'].head())
+
+# Replace the number rank by a string
+names['Rank'].replace({1:'FIRST', 2:'SECOND', 3:'THIRD'}, inplace=True)
+print(names.head())
