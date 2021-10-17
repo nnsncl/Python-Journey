555939993bd0594e089ec03f934eaf1a76c0e5e5
nnsncl <nuniroland@gmail.com>
2021-10-18 00:27:54 +0200

    replace multiple values

--- efficient-core-pandas/replace-scalar-values.py
+++ efficient-core-pandas/replace-scalar-values.py
@@ -30,3 +30,10 @@ start_time = time.time()
 names['Ethnicity'].replace(['BLACK NON HISP', 'BLACK NON HISPANIC', 'WHITE NON HISP' , 'WHITE NON HISPANIC'], 'NON HISPANIC', inplace=True)
 
 print("Time using .replace(): {} sec".format(time.time() - start_time))
+
+start_time = time.time()
+
+# Replace ethnicities as instructed
+names['Ethnicity'].replace(['ASIAN AND PACI','BLACK NON HISP', 'WHITE NON HISP'], ['ASIAN AND PACIFIC ISLANDER','BLACK NON HISPANIC','WHITE NON HISPANIC'], inplace=True)
+
+print("Time using .replace(): {} sec".format(time.time() - start_time))
