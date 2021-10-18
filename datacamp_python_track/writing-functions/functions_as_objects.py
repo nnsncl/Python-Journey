62c489e5a643ab63fbc0bfebb31ee1826873e95c
nnsncl <nuniroland@gmail.com>
2021-10-18 23:46:13 +0200

    check if a fun has a docstring

--- datacamp_python_track/writing-functions/functions_as_objects.py
+++ datacamp_python_track/writing-functions/functions_as_objects.py
@@ -13,3 +13,23 @@ func_name = get_user_input()
 
 # Call the chosen function and pass "data" as an argument
 function_map['minimum'](data)
+
+def has_docstring(func):
+  """Check to see if the function 
+  `func` has a docstring.
+
+  Args:
+    func (callable): A function.
+
+  Returns:
+    bool
+  """
+  return func.__doc__ is not None
+  
+# Call has_docstring() on the load_and_plot_data() function
+ok = has_docstring(load_and_plot_data)
+
+if not ok:
+  print("load_and_plot_data() doesn't have a docstring!")
+else:
+  print("load_and_plot_data() looks ok")
