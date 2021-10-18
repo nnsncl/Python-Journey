9ae9d1cf9d66cafd19dfbde3965dd37355e3dff5
nnsncl <nuniroland@gmail.com>
2021-10-18 22:50:06 +0200

    standardize action

--- datacamp_python_track/writing-functions/best_practices.py
+++ datacamp_python_track/writing-functions/best_practices.py
@@ -52,3 +52,24 @@ def build_tooltip(function):
 print(build_tooltip(count_letter))
 print(build_tooltip(range))
 print(build_tooltip(print))
+
+
+
+def standardize(column):
+  """Standardize the values in a column.
+
+  Args:
+    column (pandas Series): The data to standardize.
+
+  Returns:
+    pandas Series: the values as z-scores
+  """
+  # Finish the function so that it returns the z-scores
+  z_score = (column - column.mean()) / column.std()
+  return z_score
+
+# Use the standardize() function to calculate the z-scores
+df['y1_z'] = standardize(df.y1_gpa)
+df['y2_z'] = standardize(df.y2_gpa)
+df['y3_z'] = standardize(df.y3_gpa)
+df['y4_z'] = standardize(df.y4_gpa)
