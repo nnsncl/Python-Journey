117e384e202ccd5564722a5073c50abdfcea49a4
nnsncl <nuniroland@gmail.com>
2021-10-18 23:49:52 +0200

    reurning functions

--- datacamp_python_track/writing-functions/functions_as_objects.py
+++ datacamp_python_track/writing-functions/functions_as_objects.py
@@ -1,9 +1,9 @@
 # Add the missing function references to the function map
 function_map = {
-  'mean': mean,
-  'std': std,
-  'minimum': minimum,
-  'maximum': maximum
+    'mean': mean,
+    'std': std,
+    'minimum': minimum,
+    'maximum': maximum
 }
 
 data = load_data()
@@ -13,6 +13,7 @@ func_name = get_user_input()
 
 # Call the chosen function and pass "data" as an argument
 function_map['minimum'](data)
+
 
 def has_docstring(func):
   """Check to see if the function 
@@ -25,7 +26,8 @@ def has_docstring(func):
     bool
   """
   return func.__doc__ is not None
-  
+
+
 # Call has_docstring() on the load_and_plot_data() function
 ok = has_docstring(load_and_plot_data)
 
@@ -33,3 +35,22 @@ if not ok:
   print("load_and_plot_data() doesn't have a docstring!")
 else:
   print("load_and_plot_data() looks ok")
+
+  def create_math_function(func_name):
+    if func_name == 'add':
+        def add(a, b):
+            return a + b
+        return add
+    elif func_name == 'subtract':
+        # Define the subtract() function
+        def subtract(a, b):
+            return a - b
+        return subtract
+    else:
+        print("I don't know that one")
+
+add = create_math_function('add')
+print('5 + 2 = {}'.format(add(5, 2)))
+
+subtract = create_math_function('subtract')
+print('5 - 2 = {}'.format(subtract(5, 2)))
