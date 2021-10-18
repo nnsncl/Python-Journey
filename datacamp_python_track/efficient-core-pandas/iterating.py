07c11ea63ba651b1f83aa7031639a404d56f22a6
nnsncl <nuniroland@gmail.com>
2021-10-18 11:22:31 +0200

    dataframe vectorization

--- datacamp_python_track/efficient-core-pandas/iterating.py
+++ datacamp_python_track/efficient-core-pandas/iterating.py
@@ -50,3 +50,16 @@ col_start_time = time.time()
 mean_c = poker_hands[['R1', 'R2', 'R3', 'R4', 'R5']].mean(axis=0)
 print("Time using pandas vectorization for columns: {} sec".format(time.time() - col_start_time))
 print(mean_c.head())
+
+
+# Calculate the variance in each hand
+start_time = time.time()
+poker_var = poker_hands[['R1', 'R2', 'R3', 'R4', 'R5']].var(axis=1)
+print("Time using pandas vectorization: {} sec".format(time.time() - start_time))
+print(poker_var.head())
+
+# Calculate the variance in each hand
+start_time = time.time()
+poker_var = poker_hands[['R1', 'R2', 'R3', 'R4', 'R5']].values.var(axis=1, ddof=1)
+print("Time using NumPy vectorization: {} sec".format(time.time() - start_time))
+print(poker_var[0:5])
