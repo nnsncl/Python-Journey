13ccdbce962e6ae0c9e71ff196b54ec0865d86a2
nnsncl <nuniroland@gmail.com>
2021-10-18 12:14:35 +0200

    Data filtration

--- datacamp_python_track/efficient-core-pandas/data_manipulation.py
+++ datacamp_python_track/efficient-core-pandas/data_manipulation.py
@@ -36,3 +36,18 @@ restaurant_grouped = restaurant_data.groupby('time')
 # Apply the transformation
 restaurant_impute = restaurant_grouped.transform(missing_trans)
 print(restaurant_impute.head())
+
+# Filter the days where the count of total_bill is greater than $40
+total_bill_40 = restaurant_data.groupby('day').filter(lambda x: x['total_bill'].count() > 40)
+
+# Print the number of tables where total_bill is greater than $40
+print('Number of tables where total_bill is greater than $40:', total_bill_40.shape[0])
+
+# Filter the days where the count of total_bill is greater than $40
+total_bill_40 = restaurant_data.groupby('day').filter(lambda x: x['total_bill'].count() > 40)
+
+# Select only the entries that have a mean total_bill greater than $20
+total_bill_20 = total_bill_40.groupby('day').filter(lambda x : x['total_bill'].mean() > 20)
+
+# Print days of the week that have a mean total_bill greater than $20
+print('Days of the week that have a mean total_bill greater than $20:', total_bill_20.day.unique())
