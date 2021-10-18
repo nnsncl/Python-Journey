111aa85dd3103032a33a546ca91ce6a393346e18
nnsncl <nuniroland@gmail.com>
2021-10-18 23:58:13 +0200

    allow a global var to be updated in func scope globally w global keyword

--- datacamp_python_track/writing-functions/functions_as_objects.py
+++ datacamp_python_track/writing-functions/functions_as_objects.py
@@ -54,3 +54,18 @@ print('5 + 2 = {}'.format(add(5, 2)))
 
 subtract = create_math_function('subtract')
 print('5 - 2 = {}'.format(subtract(5, 2)))
+
+
+call_count = 0
+
+def my_function():
+  # Use a keyword that lets us update call_count 
+  global call_count
+  call_count += 1
+  
+  print("You've called my_function() {} times!".format(
+    call_count
+  ))
+  
+for _ in range(20):
+  my_function()
