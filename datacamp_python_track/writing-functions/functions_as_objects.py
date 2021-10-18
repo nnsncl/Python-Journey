63e355244091c4ca25be608cfea908c2f1c74427
nnsncl <nuniroland@gmail.com>
2021-10-19 00:12:41 +0200

    Check if closure extists

--- datacamp_python_track/writing-functions/functions_as_objects.py
+++ datacamp_python_track/writing-functions/functions_as_objects.py
@@ -105,3 +105,14 @@ def read_files():
   return file_contents
 
 print('\n'.join(read_files()))
+
+def return_a_func(arg1, arg2):
+  def new_func():
+    print('arg1 was {}'.format(arg1))
+    print('arg2 was {}'.format(arg2))
+  return new_func
+    
+my_func = return_a_func(2, 17)
+
+# Show that my_func()'s closure is not None
+print(my_func.__closure__ is not None)
