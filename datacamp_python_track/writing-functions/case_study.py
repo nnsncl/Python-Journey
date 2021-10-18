95d46eacaef51186de664ad94a02d5516f56f2fb
nnsncl <nuniroland@gmail.com>
2021-10-19 01:00:45 +0200

    run_n_times decorators w/ args

--- datacamp_python_track/writing-functions/case_study.py
+++ datacamp_python_track/writing-functions/case_study.py
@@ -100,9 +100,27 @@ print('Decorated time: {:.5f}s'.format(decorated_time))
 print('Undecorated time: {:.5f}s'.format(undecorated_time))
 
 
+def run_n_times(n):
+  """Define and return a decorator"""
+  def decorator(func):
+    def wrapper(*args, **kwargs):
+      for i in range(n):
+        func(*args, **kwargs)
+    return wrapper
+  return decorator
+
 # Make print_sum() run 10 times with the run_n_times() decorator
 @run_n_times(10)
 def print_sum(a, b):
   print(a + b)
   
 print_sum(15, 20)
+
+# Use run_n_times() to create the run_five_times() decorator
+run_five_times = run_n_times(5)
+
+@run_five_times
+def print_sum(a, b):
+  print(a + b)
+  
+print_sum(4, 100)
