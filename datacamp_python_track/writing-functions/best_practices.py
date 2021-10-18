8ad7b255b9ec24405e4b8ed15b2e4279b639f4c1
nnsncl <nuniroland@gmail.com>
2021-10-18 22:55:58 +0200

    calculate median

--- datacamp_python_track/writing-functions/best_practices.py
+++ datacamp_python_track/writing-functions/best_practices.py
@@ -73,3 +73,21 @@ df['y1_z'] = standardize(df.y1_gpa)
 df['y2_z'] = standardize(df.y2_gpa)
 df['y3_z'] = standardize(df.y3_gpa)
 df['y4_z'] = standardize(df.y4_gpa)
+
+
+def median(values):
+  """Get the median of a sorted list of values
+
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  # Write the median() function
+  midpoint = int(len(values) / 2)
+  if len(values) % 2 == 0:
+    median = (values[midpoint - 1] + values[midpoint]) / 2
+  else:
+    median = values[midpoint]
+  return median
