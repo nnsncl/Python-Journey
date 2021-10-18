35b8bbeb8ed82b60026fab35352bea82c1f555b2
nnsncl <nuniroland@gmail.com>
2021-10-19 00:03:01 +0200

    scope practice

--- datacamp_python_track/writing-functions/functions_as_objects.py
+++ datacamp_python_track/writing-functions/functions_as_objects.py
@@ -87,3 +87,21 @@ for _ in range(20):
   return file_contents
 
 print('\n'.join(read_files()))
+
+def read_files():
+  file_contents = None
+  
+  def save_contents(filename):
+    # Add a keyword that lets us modify file_contents
+    nonlocal file_contents
+    if file_contents is None:
+      file_contents = []
+    with open(filename) as fin:
+      file_contents.append(fin.read())
+      
+  for filename in ['1984.txt', 'MobyDick.txt', 'CatsEye.txt']:
+    save_contents(filename)
+    
+  return file_contents
+
+print('\n'.join(read_files()))
