deae83e1a6715ad91e0bf943eb6a3efea36d6a7e
nnsncl <nuniroland@gmail.com>
2021-10-20 16:14:30 +0200

    var names best practices

--- datacamp_python_track/software_engineering/_misc.py
+++ datacamp_python_track/software_engineering/_misc.py
@@ -98,3 +98,13 @@ def hypotenuse_length(leg_a, leg_b):
 
 # Print the length of the hypotenuse with legs 6 & 8
 print(hypotenuse_length(6, 8))
+
+from statistics import mean
+
+# Sample measurements of pupil diameter in mm
+pupil_diameter = [3.3, 6.8, 7.0, 5.4, 2.7]
+
+# Average pupil diameter from sample
+mean_diameter = mean(pupil_diameter)
+
+print(mean_diameter)
