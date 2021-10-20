5849f521825564287fd7d3cb17c62e58569a6dc5
nnsncl <nuniroland@gmail.com>
2021-10-20 15:22:42 +0200

    Create a child class

--- datacamp_python_track/software_engineering/text_analyser/document.py
+++ datacamp_python_track/software_engineering/text_analyser/document.py
@@ -24,3 +24,18 @@ class Document:
     def _count_words(self):
         return Counter(self.tokens)
 
+
+# Define a SocialMedia class that is a child of the `Document class`
+class SocialMedia(Document):
+    def __init__(self, text):
+        Document.__init__(self, text)
+        self.hashtag_counts = self._count_hashtags()
+        self.mention_counts = self._count_mentions()
+        
+    def _count_hashtags(self):
+        # Filter attribute so only words starting with '#' remain
+        return filter_word_counts(self.word_counts, first_char='#')      
+    
+    def _count_mentions(self):
+        # Filter attribute so only words starting with '@' remain
+        return filter_word_counts(self.word_counts, first_char='@')
