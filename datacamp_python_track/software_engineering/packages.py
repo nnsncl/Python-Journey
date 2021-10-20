8b8e1b6dcdfce52aaf7f43248e3e0bfa9cf091b9
nnsncl <nuniroland@gmail.com>
2021-10-20 14:23:38 +0200

    import and use the package

--- datacamp_python_track/software_engineering/packages.py
+++ datacamp_python_track/software_engineering/packages.py
@@ -1,7 +1,8 @@
-py_package# Import local packages
-import package
-import py_package
+# Import local package
+import text_analyzer
 
-# View the help for each package
-help(package)
-help(py_package)
+# Sum word_counts using sum_counters from text_analyzer
+word_count_totals = text_analyzer.sum_counters(word_counts)
+
+# Plot word_count_totals using plot_counter from text_analyzer
+text_analyzer.plot_counter(word_count_totals)
