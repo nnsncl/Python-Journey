40016bf7f6074a0760d6bdcdbe81a7205564b377
nnsncl <nuniroland@gmail.com>
2021-10-20 15:30:23 +0200

    use new subclass

--- datacamp_python_track/software_engineering/packages.py
+++ datacamp_python_track/software_engineering/packages.py
@@ -25,3 +25,16 @@ print(datacamp_doc.tokens[:5])
 
 # print the top 5 most used words in datacamp_doc
 print(datacamp_doc._count_words().most_common(5))
+
+
+# Import custom text_analyzer package
+import text_analyzer
+
+# Create a SocialMedia instance with datacamp_tweets
+dc_tweets = text_analyzer.SocialMedia(text=datacamp_tweets)
+
+# Print the top five most most mentioned users
+print(dc_tweets._count_mentions().most_common(5))
+
+# Plot the most used hashtags
+text_analyzer.plot_counter(dc_tweets._count_hashtags())
