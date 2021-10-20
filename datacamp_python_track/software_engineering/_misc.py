a270ac1beddb70612ec402e81a23d1e069c567e0
nnsncl <nuniroland@gmail.com>
2021-10-20 16:18:41 +0200

    function refactor w/ best practices

--- datacamp_python_track/software_engineering/_misc.py
+++ datacamp_python_track/software_engineering/_misc.py
@@ -108,3 +108,31 @@ pupil_diameter = [3.3, 6.8, 7.0, 5.4, 2.7]
 mean_diameter = mean(pupil_diameter)
 
 print(mean_diameter)
+
+import math
+
+def polygon_area(n_sides, side_len):
+    """Find the area of a regular polygon
+
+    :param n_sides: number of sides
+    :param side_len: length of polygon sides
+    :return: area of polygon
+
+    >>> round(polygon_area(4, 5))
+    25
+    """
+    def polygon_perimeter(n_sides, side_len):
+        return n_sides * side_len
+
+    def polygon_apothem(n_sides, side_len):
+        denominator = 2 * math.tan(math.pi / n_sides)
+        return side_len / denominator
+
+    def polygon_area(n_sides, side_len):
+        perimeter = polygon_perimeter(n_sides, side_len)
+        apothem = polygon_apothem(n_sides, side_len)
+
+        return perimeter * apothem / 2
+
+# Print the area of a hexagon with legs of size 10
+print(polygon_area(n_sides=6, side_len=10))
