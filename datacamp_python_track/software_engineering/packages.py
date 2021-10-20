fdfce83348c04fac204ac08e70614f8c8063cc7f
nnsncl <nuniroland@gmail.com>
2021-10-20 15:02:28 +0200

    use class on package

--- datacamp_python_track/software_engineering/packages.py
+++ datacamp_python_track/software_engineering/packages.py
@@ -1,5 +1,7 @@
-# Import local package
+# Import local packages
+from text_analyser import Document
 import text_analyzer
+
 
 # Sum word_counts using sum_counters from text_analyzer
 word_count_totals = text_analyzer.sum_counters(word_counts)
@@ -13,3 +15,13 @@ my_document = text_analyzer.Document(text=datacamp_tweet)
 
 # Print the text attribute of the Document instance
 print(my_document.text)
+
+
+# create a new document instance from datacamp_tweets
+datacamp_doc = Document(datacamp_tweets)
+
+# print the first 5 tokens from datacamp_doc
+print(datacamp_doc.tokens[:5])
+
+# print the top 5 most used words in datacamp_doc
+print(datacamp_doc._count_words().most_common(5))
