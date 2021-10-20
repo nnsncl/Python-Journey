7169b99e62422cc7e49031915992a565c07ec6cc
nnsncl <nuniroland@gmail.com>
2021-10-20 16:27:04 +0200

    document sum_counterd

--- datacamp_python_track/software_engineering/text_analyser/counter_utils.py
+++ datacamp_python_track/software_engineering/text_analyser/counter_utils.py
@@ -1,5 +1,6 @@
 # Import needed functionality
 from collections import Counter
+import doctest
 
 def plot_counter(counter, n_most_common=5):
   # Subset the n_most_common items from the input counter
@@ -9,5 +10,18 @@ def plot_counter(counter, n_most_common=5):
 
 
 def sum_counters(counters):
+  """Aggregate collections.Counter objects by summing counts
+
+    :param counters: list/tuple of counters to sum
+    :return: aggregated counters with counts summed
+
+    >>> d1 = text_analyzer.Document('1 2 fizz 4 buzz fizz 7 8')
+    >>> d2 = text_analyzer.Document('fizz buzz 11 fizz 13 14')
+    >>> sum_counters([d1.word_counts, d2.word_counts])
+    Counter({'fizz': 4, 'buzz': 2})
+  """
+
   # Sum the inputted counters
   return sum(counters, Counter())
+
+doctest.testmod()
