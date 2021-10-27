dd52b164518e85fee495ab748938d5d717231541
nnsncl <nuniroland@gmail.com>
2021-10-27 21:43:42 +0200

    subqueries basics

---
+++ datacamp_sql_track/subqueries/subqueries.sql
@@ -0,0 +1,22 @@
+-- Select fields
+SELECT *
+  -- From populations
+  FROM populations
+-- Where life_expectancy is greater than
+WHERE life_expectancy >
+  -- 1.15 * subquery
+  1.15 * (SELECT AVG(life_expectancy)
+   FROM populations
+   WHERE year = 2015) AND
+  year = 2015;
+
+  -- Select fields
+SELECT name, country_code, urbanarea_pop
+  -- From cities
+  FROM cities
+-- Where city name in the field of capital cities
+WHERE name IN
+  -- Subquery
+  (SELECT capital
+   FROM countries)
+ORDER BY urbanarea_pop DESC;
