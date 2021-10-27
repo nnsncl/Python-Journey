4353980737954182432974289e5887bc70e38868
nnsncl <nuniroland@gmail.com>
2021-10-27 17:20:31 +0200

    EXCEPT basics and practice

--- datacamp_sql_track/set_theory/set_thory.sql
+++ datacamp_sql_track/set_theory/set_thory.sql
@@ -49,3 +49,40 @@ SELECT country_code, year
   FROM populations
 -- Order by code and year
 ORDER BY code, year;
+
+-- Select fields
+SELECT code, name
+  -- From countries
+  FROM countries
+	-- Set theory clause
+	INTERSECT
+-- Select fields
+SELECT country_code, name
+  -- From cities
+  FROM cities;
+
+  -- Select field
+SELECT name
+  -- From cities
+  FROM cities
+	-- Set theory clause
+	EXCEPT
+-- Select field
+SELECT capital
+  -- From countries
+  FROM countries
+-- Order by result
+ORDER BY name;
+
+-- Select field
+SELECT capital
+  -- From countries
+  FROM countries
+	-- Set theory clause
+	EXCEPT
+-- Select field
+SELECT name
+  -- From cities
+  FROM cities
+-- Order by ascending capital
+ORDER BY capital;
