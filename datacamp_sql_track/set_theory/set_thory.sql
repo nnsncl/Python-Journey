2e9825f208f4cf7ed3507571ba7c29b4b9f9b46e
nnsncl <nuniroland@gmail.com>
2021-10-27 17:05:59 +0200

    Practice UNION

--- datacamp_sql_track/set_theory/set_thory.sql
+++ datacamp_sql_track/set_theory/set_thory.sql
@@ -10,3 +10,16 @@ SELECT *
   FROm economies2015
 -- Order by code and year
 ORDER BY code, year;
+
+-- Select field
+SELECT country_code
+  -- From cities
+  FROM cities
+	-- Set theory clause
+	UNION
+-- Select field
+SELECT code
+  -- From currencies
+  FROM currencies
+-- Order by country_code
+ORDER BY country_code;
