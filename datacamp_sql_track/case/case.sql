ea2d9647104455315c6290a5ce45d9d411233eb0
nnsncl <nuniroland@gmail.com>
2021-10-28 21:56:47 +0200

    Advanced Case w/multiple steps

--- datacamp_sql_track/case/case.sql
+++ datacamp_sql_track/case/case.sql
@@ -94,3 +94,38 @@ FROM matches_spain
 WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
       AND (awayteam_id = 8633 OR hometeam_id = 8633);
       
+
+-- Select team_long_name and team_api_id from team
+SELECT
+	team_long_name,
+	team_api_id
+FROM team
+-- Filter for team long name
+WHERE team_long_name = 'Bologna';
+
+-- Select the season and date columns
+SELECT 
+	season,
+	date,
+    -- Identify when Bologna won a match
+	CASE WHEN hometeam_id = 9857 
+         AND home_goal > away_goal 
+         THEN 'Bologna Win'
+		 WHEN awayteam_id = 9857 
+         AND away_goal > home_goal 
+         THEN 'Bologna Win' 
+		 END AS outcome
+FROM matches_italy;
+
+-- Select the season, date, home_goal, and away_goal columns
+SELECT 
+	season,
+    date,
+	home_goal,
+	away_goal
+FROM matches_italy
+WHERE 
+-- Exclude games not won by Bologna
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		 WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win' 
+		END IS NOT NULL;
