4dc36cc74408e1921150f89c19b82f68d4d34fb3
nnsncl <nuniroland@gmail.com>
2021-11-11 00:04:39 +0100

    yay pyspark

---
+++ datacamp_data_engineer_track/pyspark.py
@@ -0,0 +1,32 @@
+# The Spark Dataframe, athlete_events_spark is loaded on jupyter
+# Print the type of athlete_events_spark
+print(type(athlete_events_spark))
+
+# Print the schema of athlete_events_spark
+print(athlete_events_spark.printSchema())
+
+# Group by the Year, and find the mean Age
+print(athlete_events_spark.groupBy('Year').mean('Age'))
+
+# Group by the Year, and find the mean Age
+print(athlete_events_spark.groupBy('Year').mean('Age').show())
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.getOrCreate()
+    athlete_events_spark = (spark
+        .read
+        .csv("/home/repl/datasets/athlete_events.csv",
+             header=True,
+             inferSchema=True,
+             escape='"'))
+
+    athlete_events_spark = (athlete_events_spark
+        .withColumn("Height",
+                    athlete_events_spark.Height.cast("integer")))
+
+    print(athlete_events_spark
+        .groupBy('Year')
+        .mean('Height')
+        .orderBy('Year')
+        .show())
