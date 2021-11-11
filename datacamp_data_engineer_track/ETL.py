20b3ba50e6619d5f21682d61396ba712bcb01b42
nnsncl <nuniroland@gmail.com>
2021-11-11 19:55:51 +0100

    Transform data w/ PySpark

--- datacamp_data_engineer_track/ETL.py
+++ datacamp_data_engineer_track/ETL.py
@@ -28,3 +28,29 @@ extract_table_to_pandas("film", db_engine)
 
 # Extract the customer table into a pandas DataFrame
 extract_table_to_pandas("customer", db_engine)
+
+
+#Transform
+# Get the rental rate column as a string
+rental_rate_str = film_df.rental_rate.astype("str")
+
+# Split up and expand the column
+rental_rate_expanded = rental_rate_str.str.split(".", expand=True)
+
+# Assign the columns to film_df
+film_df = film_df.assign(
+    rental_rate_dollar=rental_rate_expanded[0],
+    rental_rate_cents=rental_rate_expanded[1],
+)
+
+# Use groupBy and mean to aggregate the column
+ratings_per_film_df = rating_df.groupBy('film_id').mean('rating')
+
+# Join the tables using the film_id column
+film_df_with_ratings = film_df.join(
+    ratings_per_film_df,
+    film_df.film_id==ratings_per_film_df.film_id
+)
+
+# Show the 5 first results
+print(film_df_with_ratings.show(5))
