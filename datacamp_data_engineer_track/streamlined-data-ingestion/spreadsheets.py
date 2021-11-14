179b04c82f1521a71366f0b8bfd78fef0858ee2e
nnsncl <nuniroland@gmail.com>
2021-11-14 18:59:21 +0100

    Parse datimetimes

--- datacamp_data_engineer_track/streamlined-data-ingestion/spreadsheets.py
+++ datacamp_data_engineer_track/streamlined-data-ingestion/spreadsheets.py
@@ -86,3 +86,21 @@ survey_subset = pd.read_excel("fcc_survey_yn_data.xlsx",
 
 # View the data
 print(survey_subset.head())
+
+# Load file, with Part1StartTime parsed as datetime data
+survey_data = pd.read_excel("fcc_survey.xlsx",
+                            parse_dates=["Part1StartTime"])
+
+# Print first few values of Part1StartTime
+print(survey_data.Part1StartTime.head())
+
+# Create dict of columns to combine into new datetime column
+datetime_cols = {"Part2Start": ["Part2StartDate", "Part2StartTime"]}
+
+
+# Load file, supplying the dict to parse_dates
+survey_data = pd.read_excel("fcc_survey_dts.xlsx",
+                            parse_dates=datetime_cols)
+
+# View summary statistics about Part2Start
+print(survey_data.Part2Start.describe())
