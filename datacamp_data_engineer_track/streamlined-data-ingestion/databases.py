2bc5d84db2a2877aa6ad636f9e216e6306a64a72
nnsncl <nuniroland@gmail.com>
2021-11-14 19:43:45 +0100

    Functions aggregation

--- datacamp_data_engineer_track/streamlined-data-ingestion/databases.py
+++ datacamp_data_engineer_track/streamlined-data-ingestion/databases.py
@@ -62,3 +62,47 @@ SELECT *
 wintry_days = pd.read_sql(query, engine)
 # View summary stats about the temperatures
 print(wintry_days.describe())
+
+# Create query for unique combinations of borough and complaint_type
+query = """
+SELECT DISTINCT borough, 
+       complaint_type
+  FROM hpd311calls;
+"""
+
+# Load results of query to a data frame
+issues_and_boros = pd.read_sql(query, engine)
+
+# Check assumption about issues and boroughs
+print(issues_and_boros)
+
+# Create query to get call counts by complaint_type
+query = """
+SELECT complaint_type, 
+     COUNT(*)
+  FROM hpd311calls
+  GROUP BY complaint_type;
+"""
+
+# Create data frame of call counts by issue
+calls_by_issue = pd.read_sql(query, engine)
+
+# Graph the number of calls for each housing issue
+calls_by_issue.plot.barh(x="complaint_type")
+plt.show()
+
+# Create query to get temperature and precipitation by month
+query = """
+SELECT month, 
+        MAX(tmax), 
+        MIN(tmin),
+        SUM(prcp)
+  FROM weather 
+ GROUP BY month;
+"""
+
+# Get data frame of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
