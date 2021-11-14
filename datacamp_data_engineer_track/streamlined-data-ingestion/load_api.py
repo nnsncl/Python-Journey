9e85182ddd626fc38a5044c9574bb0b6e2301d72
nnsncl <nuniroland@gmail.com>
2021-11-14 22:33:39 +0100

    Merging dfs

--- datacamp_data_engineer_track/streamlined-data-ingestion/load_api.py
+++ datacamp_data_engineer_track/streamlined-data-ingestion/load_api.py
@@ -73,3 +73,17 @@ flat_cafes = json_normalize(data["businesses"],
 
 # View the data
 print(flat_cafes.head())
+
+# Merge crosswalk into cafes on their zip code fields
+cafes_with_pumas = cafes.merge(crosswalk,
+                                left_on='location_zip_code',
+                                right_on='zipcode')
+
+
+
+# Merge pop_data into cafes_with_pumas on puma field
+cafes_with_pop = cafes_with_pumas.merge(pop_data,
+                                        left_on='puma',
+                                        right_on='puma')
+# View the data
+print(cafes_with_pop.head())
