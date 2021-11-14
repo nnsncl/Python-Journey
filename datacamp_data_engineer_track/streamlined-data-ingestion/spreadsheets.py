4ae43d677a754f447f4d6f3d2a9d546f9aee8c79
nnsncl <nuniroland@gmail.com>
2021-11-14 13:56:21 +0100

    Load a spreadsheet chunk

--- datacamp_data_engineer_track/streamlined-data-ingestion/spreadsheets.py
+++ datacamp_data_engineer_track/streamlined-data-ingestion/spreadsheets.py
@@ -6,3 +6,14 @@ survey_responses = pd.read_excel("fcc_survey.xlsx")
 
 # View the head of the data frame
 print(survey_responses.head())
+
+# Create string of lettered columns to load
+col_string = "AD, AW:BA"
+
+# Load data with skiprows and usecols set
+survey_responses = pd.read_excel("fcc_survey_headers.xlsx", 
+                        skiprows=2, 
+                        usecols=col_string )
+
+# View the names of the columns selected
+print(survey_responses.columns)
