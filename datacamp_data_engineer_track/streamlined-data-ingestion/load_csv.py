649e73616a9a08d4dc0203950834aefeae98d32d
nnsncl <nuniroland@gmail.com>
2021-11-14 13:47:13 +0100

    Skip currupted data

--- datacamp_data_engineer_track/streamlined-data-ingestion/load_csv.py
+++ datacamp_data_engineer_track/streamlined-data-ingestion/load_csv.py
@@ -58,3 +58,15 @@ data = pd.read_csv("vt_tax_data_2016.csv",
 
 # View rows with NA ZIP codes
 print(data[data.zipcode.isna()])
+
+try:
+  # Set warn_bad_lines to issue warnings about bad records
+  data = pd.read_csv("vt_tax_data_2016_corrupt.csv", 
+                     error_bad_lines=False, 
+                     warn_bad_lines=True)
+  
+  # View first 5 records
+  print(data.head())
+  
+except pd.io.common.CParserError:
+    print("Your data contained rows that could not be parsed.")
