2c1929e424d8cbe18370bcc0d31b530edc7f055d
nnsncl <nuniroland@gmail.com>
2021-11-17 00:01:57 +0100

    Download data w/ wget and curl

--- datacamp_data_engineer_track/data_processing_in_shell/wget.sh
+++ datacamp_data_engineer_track/data_processing_in_shell/wget.sh
@@ -6,3 +6,12 @@ ls
 
 # Preview the log file 
 cat wget-log
+
+# View url_list.txt to verify content
+cat url_list.txt
+
+# Create a mandatory 1 second pause between downloading all files in url_list.txt
+wget --wait=1 -i url_list.txt
+
+# Take a look at all files downloaded
+ls
