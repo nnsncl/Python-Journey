645d8c7f2af177a5b60bd5283bcc773ce42c1141
nnsncl <nuniroland@gmail.com>
2021-11-16 23:28:43 +0100

    download data chunks

--- datacamp_data_engineer_track/data_processing_in_shell/curl.sh
+++ datacamp_data_engineer_track/data_processing_in_shell/curl.sh
@@ -1,2 +1,7 @@
 # Use curl to Download and rename the file in the same step from the redirected URL
 curl -o Spotify201812.zip -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+
+# Download all 100 data files
+curl -O https://s3.amazonaws.com/assets.datacamp.com/production/repositories/4180/datasets/files/datafile[0-100].txt
+# Print all downloaded files to directory
+ls datafile*.txt
