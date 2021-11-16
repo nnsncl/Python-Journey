22cb1a46254c355cb8415a199945abdf22a1e314
nnsncl <nuniroland@gmail.com>
2021-11-16 23:29:32 +0100

    update perimeter variable

--- datacamp_data_engineer_track/data_processing_in_shell/curl.sh
+++ datacamp_data_engineer_track/data_processing_in_shell/curl.sh
@@ -2,6 +2,6 @@
 curl -o Spotify201812.zip -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
 
 # Download all 100 data files
-curl -O https://s3.amazonaws.com/assets.datacamp.com/production/repositories/4180/datasets/files/datafile[0-100].txt
+curl -O https://s3.amazonaws.com/assets.datacamp.com/production/repositories/4180/datasets/files/datafile[001-100].txt
 # Print all downloaded files to directory
 ls datafile*.txt
