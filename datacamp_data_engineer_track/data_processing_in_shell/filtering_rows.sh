2b3ec5db66cf1a034ea28cc204dab18f14de3411
nnsncl <nuniroland@gmail.com>
2021-11-18 21:38:05 +0100

    Filter by id w/ csvgrep

--- datacamp_data_engineer_track/data_processing_in_shell/filtering_rows.sh
+++ datacamp_data_engineer_track/data_processing_in_shell/filtering_rows.sh
@@ -12,3 +12,9 @@ csvcut -c "track_id" Spotify_MusicAttributes.csv
 
 # Print the track id, song duration, and loudness, by name 
 csvcut -c "track_id","duration_ms","loudness" Spotify_MusicAttributes.csv
+
+# Filter for row(s) where track_id = 118GQ70Sp6pMqn6w1oKuki
+csvgrep -c "track_id" -m 118GQ70Sp6pMqn6w1oKuki Spotify_MusicAttributes.csv
+
+# Filter for row(s) where danceability = 0.812
+csvgrep -c "danceability" -m 0.812 Spotify_MusicAttributes.csv
