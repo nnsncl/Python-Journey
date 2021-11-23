b362312c4c4f28ec20a78be84364244c18bb6666
nnsncl <nuniroland@gmail.com>
2021-11-23 22:19:43 +0100

    redirect to a nex file

--- datacamp_data_engineer_track/data_processing_in_shell/sql_local_file.sh
+++ datacamp_data_engineer_track/data_processing_in_shell/sql_local_file.sh
@@ -2,3 +2,9 @@
 csvsql --query "SELECT * FROM Spotify_MusicAttributes ORDER BY duration_ms LIMIT 1" \
 	Spotify_MusicAttributes.csv | csvlook
 
+# Re-direct output to new file: LongestSong.csv
+csvsql --query "SELECT * FROM Spotify_MusicAttributes ORDER BY duration_ms LIMIT 1" \
+	Spotify_MusicAttributes.csv > LongestSong.csv
+    
+# Preview newly created file 
+csvlook LongestSong.csv
