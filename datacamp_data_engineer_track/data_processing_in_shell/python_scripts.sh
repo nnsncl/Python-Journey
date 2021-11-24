1882f8b691e91c99ea25d6f822d66d2eed85581f
nnsncl <nuniroland@gmail.com>
2021-11-24 20:45:49 +0100

    automate python deps install w/shell

--- datacamp_data_engineer_track/data_processing_in_shell/python_scripts.sh
+++ datacamp_data_engineer_track/data_processing_in_shell/python_scripts.sh
@@ -11,3 +11,15 @@ cat my_first_python_script.py
 python my_first_python_script.py
 
 rm my_first_python_script.py
+
+# Add scikit-learn to the requirements.txt file
+echo "scikit-learn" > requirements.txt
+
+# Preview file content
+cat requirements.txt
+
+# Install the required dependencies
+pip install -r requirements.txt
+
+# Verify that Scikit-Learn is now installed
+pip list
