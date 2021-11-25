2ca118962a3643cfb0758e6321fd9a0af37621e3
nnsncl <nuniroland@gmail.com>
2021-11-25 20:54:50 +0100

    Intro to bash scripting

--- datacamp_data_engineer_track/bash_basics/dates.sh
+++ datacamp_data_engineer_track/bash_basics/dates.sh
@@ -1,1 +1,7 @@
-cut -d , -f 1 seasonal/*.csv
+#!/bin/bash
+
+# Create a single-line pipe
+cat soccer_scores.csv | cut -d "," -f 2 | tail -n +2 | sort | uniq -c
+
+# Create a sed pipe to a new file
+cat soccer_scores.csv | sed 's/Cherno/Cherno City/g' | sed 's/Arda/Arda United/g' > soccer_scores_edited.csv
