717a1929a8d25007728a1a0ea158c43153dd5b7a
nnsncl <nuniroland@gmail.com>
2021-11-29 12:13:20 +0100

    print out arrays keys

--- datacamp_data_engineer_track/bash_scripting/arrays.sh
+++ datacamp_data_engineer_track/bash_scripting/arrays.sh
@@ -28,3 +28,5 @@ declare -A model_metrics=([model_accuracy]=98 [model_name]="knn" [model_f1]=0.82
 
 # Print out the entire array
 echo ${model_metrics[@]}
+# Print out just the keys
+echo ${!model_metrics[@]}
