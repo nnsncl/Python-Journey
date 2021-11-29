b5649f682980304edc141be088a709aa7a5b80ba
nnsncl <nuniroland@gmail.com>
2021-11-29 12:12:35 +0100

    Declare an associative array w/ a one-liner script

--- datacamp_data_engineer_track/bash_scripting/arrays.sh
+++ datacamp_data_engineer_track/bash_scripting/arrays.sh
@@ -22,3 +22,9 @@ declare -A model_metrics
 model_metrics[model_accuracy]=98
 model_metrics[model_name]="knn"
 model_metrics[model_f1]=0.82
+
+# Declare associative array with key-value pairs on one line
+declare -A model_metrics=([model_accuracy]=98 [model_name]="knn" [model_f1]=0.82)
+
+# Print out the entire array
+echo ${model_metrics[@]}
