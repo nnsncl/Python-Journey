d5780123ac105edca2d3b81412fc18292ebef7db
nnsncl <nuniroland@gmail.com>
2021-11-29 12:04:58 +0100

    Append to arrays

---
+++ datacamp_data_engineer_track/bash_scripting/arrays.sh
@@ -0,0 +1,7 @@
+# Create a normal array with the mentioned elements using the declare method
+declare -a capital_cities
+
+# Add (append) the elements
+capital_cities[0]="Sydney"
+capital_cities[1]="Sydney"
+capital_cities+=("Paris")
